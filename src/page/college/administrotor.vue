a47adeeb26b64313450204e30dcabaff13678d3e
tonylzm <97021362+tonylzm@users.noreply.github.com>
2024-05-15 11:46:38 +0800

    优化

--- src/page/college/administrotor.vue
+++ src/page/college/administrotor.vue
@@ -234,7 +234,6 @@ export default {
                         checkpassword: '',
                         tel: '',
                         email: '',
-
                     };
                     done();
                 })
